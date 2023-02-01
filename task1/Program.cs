9751bfee97060b997ac36ea2eee40a315868325a
Alen <Spookyshocky@ya.ru>
2023-02-01 21:19:27 +0100

    сделал домашку для семинара 4 было сложно

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+void main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число A: ");
+    int cum = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Введите число B: ");
+    int too = int.Parse(Console.ReadLine()!);
+
+    int result = 1;
+    int count = 1;
+    while (count <= too)
+    {
+        result = result * cum;
+        count++;
+    }
+
+    Console.WriteLine(result);
+}
+
+main();
+
---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = Console.ReadLine()
+                .Split(' ', ',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+Console.WriteLine(string.Join(", ", arr));
